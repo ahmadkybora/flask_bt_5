93f3c688ed24e1a97463bbacf8c2c2cefb2daa56
ahmadkybora <ahmadkybora@gmail.com>
2022-02-25 20:30:45 +0330

    =new

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
 from telegram import Update, InlineKeyboardButton
-from decouple import config
+# from decouple import config
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -9,30 +9,30 @@ logger = logging.getLogger(__name__)
 token = "2016260844:AAGwWwI6ZLA7cLUNNcAbbFz2W84wkJebZyo"
 # token = config("TOKEN")
 
-def start(update: Update, context: CallbackContext):
-    keyboards = [
-        [InlineKeyboardButton('1', callback_data='salam_1'), InlineKeyboardButton('2', callback_data='salam_2')], 
-        [InlineKeyboardButton('3', callback_data='salam_3'), InlineKeyboardButton('4', callback_data='salam_4')], 
-        [InlineKeyboardButton('5', callback_data='salam_5'), InlineKeyboardButton('6', callback_data='salam_6')], 
-        [InlineKeyboardButton('7', callback_data='salam_7'), InlineKeyboardButton('8', callback_data='salam_8')], 
-    ]
+# def start(update: Update, context: CallbackContext):
+#     keyboards = [
+#         [InlineKeyboardButton('1', callback_data='salam_1'), InlineKeyboardButton('2', callback_data='salam_2')], 
+#         [InlineKeyboardButton('3', callback_data='salam_3'), InlineKeyboardButton('4', callback_data='salam_4')], 
+#         [InlineKeyboardButton('5', callback_data='salam_5'), InlineKeyboardButton('6', callback_data='salam_6')], 
+#         [InlineKeyboardButton('7', callback_data='salam_7'), InlineKeyboardButton('8', callback_data='salam_8')], 
+#     ]
 
-    update.effective_message.reply_text('ss', reply_markup=keyboards)
+#     update.effective_message.reply_text('ss', reply_markup=keyboards)
 
-def getVoice(update: Updater, context: CallbackContext):
-    file = update.bot.get_file().download()
+# def getVoice(update: Updater, context: CallbackContext):
+#     file = update.bot.get_file().download()
 
 
 def getFile(update: Update, context: CallbackContext):
     text = update.message.text
-    file = context.bot.sendPhoto(update.effective_chat.id, text)
+    file = context.bot.sendDocument(update.effective_chat.id, text)
     update.message.reply_text(file)
-    
+
 def main():
     updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler("start", start))
+    # dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(Filters.text, getFile))
 
     updater.start_polling()

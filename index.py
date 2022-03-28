4c3640d0aadb94536a2f058fd98d5f3fb20adc63
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-03-28 16:38:05 +0330

    =new

--- index.py
+++ index.py
@@ -23,25 +23,29 @@ token = "2016260844:AAGwWwI6ZLA7cLUNNcAbbFz2W84wkJebZyo"
 #     file = update.bot.get_file().download()
 
 
-def getFile(update: Update, context: CallbackContext):
-    # text = update.message.text
-    text = "https://youtu.be/T7RNhMChbrQ"
-    # file = context.bot.send_document(update.effective_chat.id, text)
-    #file = context.bot.send_video(update.effective_chat.id, text)
-    file = context.bot.sendPhoto(update.effective_chat.id, "https://fr.dreamstime.com/photo-stock-paysage-panoramique-d-automne-courant-for%C3%AAt-backg-nature-chute-image79856609")
-    # file = context.bot.sendVideo(update.effective_chat.id, text)
-    YouTube(content).streams.first().download(path)
-    video = open(path, 'rb')
-    bot.send_video(message.chat.id, video)
-    context.bot.sendMessage(file)
+# def getFile(update: Update, context: CallbackContext):
+#     # text = update.message.text
+#     text = "https://youtu.be/T7RNhMChbrQ"
+#     # file = context.bot.send_document(update.effective_chat.id, text)
+#     #file = context.bot.send_video(update.effective_chat.id, text)
+#     file = context.bot.sendPhoto(update.effective_chat.id, "https://fr.dreamstime.com/photo-stock-paysage-panoramique-d-automne-courant-for%C3%AAt-backg-nature-chute-image79856609")
+#     # file = context.bot.sendVideo(update.effective_chat.id, text)
+#     YouTube(content).streams.first().download(path)
+#     video = open(path, 'rb')
+#     bot.send_video(message.chat.id, video)
+#     context.bot.sendMessage(file)
+
+def start(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    context.bot.send_message(chat_id, 'hello')
 
 def main():
     updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
 
     # dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text, getFile))
-
+    # dispatcher.add_handler(MessageHandler(Filters.text, getFile))
+    dispatcher.add_handler(CommandHandler("start", start))
     updater.start_polling()
     updater.idle()
 
